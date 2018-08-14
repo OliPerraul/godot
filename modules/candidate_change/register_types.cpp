8b8f258f334da6e50a3bf41d207583bc8f42213d
oliperraul <olivier.perrault@usherbrooke.ca>
2018-08-14 17:08:19 -0400

    Added candidate change types.

---
+++ modules/candidate_change/register_types.cpp
@@ -0,0 +1,14 @@
+
+#include "register_types.h"
+
+#include "tab_container.h"
+
+
+void register_candidate_change_types() {
+
+	ClassDB::register_class<TabContainer2>();
+}
+
+void unregister_candidate_change_types() {
+
+}
---
+++ modules/candidate_change/register_types.h
@@ -0,0 +1,3 @@
+
+void register_candidate_change_types();
+void unregister_candidate_change_types();
